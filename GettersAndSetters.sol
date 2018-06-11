03393056da4491128a1f64b92ecaad1eee82ae1b
Osman Toplica <ostop.developer@gmail.com>
2018-06-11 21:26:02 +0200

    Refactored GettersAndSetters.sol so it complies with the official style guide.

--- GettersAndSetters.sol
+++ GettersAndSetters.sol
@@ -1,27 +1,25 @@
 pragma solidity ^0.4.0;
 
 /*
-Basic getters and setters
+**  Basic getters and setters
 */
 contract GettersAndSetters {
-
     uint age = 24;
-    string name = "Osman";
+	string name = "Osman";
 
-   function getAge() public view returns (uint) {
+	function getAge() public view returns (uint) {
         return age;
     }
-
+    
     function getName() public view returns (string) {
         return name;
     }
-
-   function setAge (uint _age) public {
-       age = _age;
-   }
-
-   function setName(string _name) public {
-       name = _name;
-   }
-
+    
+    function setAge (uint _age) public {
+        age = _age;
+    }
+    
+    function setName(string _name) public {
+        name = _name;
+    }
 }
